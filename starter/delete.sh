051175da061b2424f79880ddda14e2ad13bad957
To Quang Duy <toduy96@gmail.com>
2024-12-11 00:49:44 +0700

    Update udagram yml

--- starter/delete.sh
+++ starter/delete.sh
@@ -1,2 +1,3 @@
-aws cloudformation delete-stack --stack-name $1 \
-    --region=us-east-1
+# aws cloudformation delete-stack --stack-name $1 \
+#     --region=us-east-1
+aws cloudformation delete-stack --stack-name $1 --region=us-east-1
